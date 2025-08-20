2b3549e78ac72f6411268a30504ecf4c554d9c6b
umer farook <umerfarook@umers-MacBook-Pro.local>
2025-08-20 18:25:07 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/sagemigrates/",
+  build: {
+    outDir: 'docs',   // 👈 build goes to docs/
+  },
 })
