a6c7ca142d3de4694ffadff95b00bed657e89416
UmerFarook <umerfarook7877@gmail.com>
2026-01-27 23:23:13 +0530

    updates

--- docs/_next/static/7l1RfeBcbjCljei4YYNw7/_buildManifest.js
+++ docs/_next/static/7l1RfeBcbjCljei4YYNw7/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/docs//_next/:path+",
-        "destination": "/_next/:path+"
+        "source": "/sagemigrates//_next/:path+",
+        "destination": "/sagemigrates/_next/:path+"
       }
     ],
     "fallback": []
