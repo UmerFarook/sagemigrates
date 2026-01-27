6e5605c3470ec91cc4995e83be94471a63f83b0c
UmerFarook <umerfarook7877@gmail.com>
2026-01-27 23:29:36 +0530

    changed folder

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
     output: 'export',
-    distDir: 'docs',
     images: { unoptimized: true },
     basePath: '/sagemigrates',
     assetPrefix: '/sagemigrates/',
